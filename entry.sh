30b1418e11e66877e451196dc4fa29d8a613027d
Baptiste Boulongne <contact@blgn.dev>
2026-03-05 00:23:52 +0100

    fix force_install_dir position

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 mkdir -p "${STEAMAPPDIR}" || true  
 
-bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
-				+force_install_dir "${STEAMAPPDIR}" \
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+				+login anonymous \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
